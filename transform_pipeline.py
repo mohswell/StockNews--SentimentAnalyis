c484851413fe638334d71d3c1db2b8131b654cb4
Muhammad Said <mohammedabdy10@gmail.com>
2024-09-11 21:56:06 +0300

    Update transform_pipeline.py

--- transform_pipeline.py
+++ transform_pipeline.py
@@ -11,8 +11,8 @@ news_data['word_count'] = news_data['text'].apply(lambda x: len(x.split()))
 print(news_data.head())
 
 # Calculate rolling averages for 'price' and 'volume' columns
-#stock_data['price_rolling_mean'] = stock_data['price'].rolling(window=7).mean()
-#stock_data['volume_rolling_mean'] = stock_data['volume'].rolling(window=7).mean()
+stock_data['price_rolling_mean'] = stock_data['price'].rolling(window=7).mean()
+stock_data['volume_rolling_mean'] = stock_data['volume'].rolling(window=7).mean()
 
 # Check the new features
-#print(stock_data.head())
+print(stock_data.head())
